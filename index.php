a779fce6219047657e57847445f3e335afe8baac
prititaliya <preetitaliya2244@gmail.com>
2022-01-10 18:55:29 +0530

    remove

--- index.php
+++ index.php
@@ -76,6 +76,5 @@ if ($_SERVER['REQUEST_METHOD']=='GET'  && isset($_GET['showerror'])){
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
 -->
-<p> hello world</p>
 </body>
 </html>
